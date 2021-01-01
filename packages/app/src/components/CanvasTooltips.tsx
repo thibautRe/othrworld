b91d58ab91bce2f4d3969960c235d2412c0109d7
Thibaut REMY <thibaut@thibaut.re>
2021-01-01 14:34:04 +0100

    chore: fix commit

--- packages/app/src/components/CanvasTooltips.tsx
+++ packages/app/src/components/CanvasTooltips.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 import { useCanvasTooltips } from '../providers/CanvasTooltipProvider'
 import { Popover } from './Popover'
 
-const CanvasPlanetTooltip = ({planet}) => {
-
-}
-
 export const CanvasTooltips = () => {
   const { canvasTooltip, onCloseCanvasTooltip } = useCanvasTooltips()
 
