acfb656f54368b27ca10e15a9868ca0cfe0ec4ca
Thibaut REMY <thibaut@thibaut.re>
2021-01-09 21:59:11 +0100

    chore: experimental react :package:

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 
 import { App } from './App'
 
-ReactDOM.render(
+// @ts-expect-error unstable_createRoot
+ReactDOM.unstable_createBlockingRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
