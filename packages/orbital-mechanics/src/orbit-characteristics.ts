f756ec38cbebeeba361d064cb5b107af1a0f53d4
Thibaut REMY <thibaut@thibaut.re>
2021-02-20 19:38:37 +0100

    chore: try to break CI

--- packages/orbital-mechanics/src/orbit-characteristics.ts
+++ packages/orbital-mechanics/src/orbit-characteristics.ts
@@ -8,7 +8,7 @@ import { G } from './utils'
 export const getPeriapsis = (orbit: Orbit) => multUnit(orbit.a, 1 - orbit.e)
 
 /** Returns the apoapsis (highest altitude) of a given orbit. Only available for elliptical orbits */
-export const getApoapsis = (orbit: OrbitEllipse) =>
+export const getApoapsis = (orbit: OrbitHyperbola) =>
   multUnit(orbit.a, 1 + orbit.e)
 
 /** Returns the semi-minor axis of a given orbit */
