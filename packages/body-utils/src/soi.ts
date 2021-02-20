d5afc8f4eeadfb5ecaa98487e6f363b7729d8c1e
Thibaut REMY <thibaut@thibaut.re>
2021-02-20 22:26:33 +0100

    chore: memoize base SOI radius

--- packages/body-utils/src/soi.ts
+++ packages/body-utils/src/soi.ts
@@ -7,10 +7,16 @@ import {
 } from '@othrworld/orbital-mechanics'
 
 import { getBodyMass } from './mass'
+import { withMemoSimple } from '@othrworld/memo-utils'
+
+// Memoized base SOI radius for the planet
+const getSOIRadiusBaseUnit = withMemoSimple((body: Planet) =>
+  Math.pow(getBodyMass(body) / (3 * body.orbit.parentMass), 1 / 3)
+)
 
 // https://en.wikipedia.org/wiki/Hill_sphere
 const getBodySOIRadiusAtDistance = (body: Planet, r: Distance): Distance =>
-  multUnit(r, Math.pow(getBodyMass(body) / (3 * body.orbit.parentMass), 1 / 3))
+  multUnit(r, getSOIRadiusBaseUnit(body))
 
 /** Radius of the SOI at a given date */
 export const getBodySOIRadius = (body: Planet, t: Date): number => {
@@ -18,18 +24,22 @@ export const getBodySOIRadius = (body: Planet, t: Date): number => {
   return getBodySOIRadiusAtDistance(body, r)
 }
 
+/** Returns the lowest SOI Radius (at periapsis) */
+export const getLowestBodySOIRadius = (body: Planet) =>
+  getBodySOIRadiusAtDistance(body, getPeriapsis(body.orbit))
+
+export const getHighestBodySOIRadius = (body: Planet) =>
+  getBodySOIRadiusAtDistance(body, getApoapsis(body.orbit))
+
 /** Return the bounds of the SOI sphere (smallest at periapsis, biggest at apoapsis) */
 /** TODO move to a body-utils package */
 export const getBodySOIRadiusBounds = (body: Planet): [number, number] => [
-  getBodySOIRadiusAtDistance(body, getPeriapsis(body.orbit)),
-  getBodySOIRadiusAtDistance(body, getApoapsis(body.orbit)),
+  getLowestBodySOIRadius(body),
+  getHighestBodySOIRadius(body),
 ]
 
 /** Returns true if the given orbit around the body is always contained within the SOI */
 export const isOrbitContainedInSOI = (body: Planet, orbit: Orbit): boolean => {
   if (isOrbitHyperbola(orbit)) return false
-  // smallest SOI radius is at periapsis
-  const soi = getBodySOIRadiusAtDistance(body, getPeriapsis(body.orbit))
-
-  return getApoapsis(orbit) < soi
+  return getApoapsis(orbit) < getLowestBodySOIRadius(body)
 }
